507c587ca9248312a00a0293636472becfb7355a
adamfoerster <adam.foerster@gmail.com>
2018-04-22 22:04:12 -0300

    basic implementation complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -13,8 +16,11 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,10 +5,77 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage{
+  form: {weeks: number, months: number} = {weeks: 1, months: 1};
 
   constructor(public navCtrl: NavController) {
-
   }
 
+  convert(weeks) {
+    if (!weeks) return null;
+    switch(weeks){
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+        this.form['months'] = 1;
+      break;
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+        this.form['months']=2;
+      break;
+      case '9':
+      case '10':
+      case '11':
+      case '12':
+        this.form['months']=3;
+      break;
+      case '13':
+      case '14':
+      case '15':
+      case '16':
+        this.form['months']=4;
+      break;
+      case '17':
+      case '18':
+      case '19':
+      case '20':
+      case '21':
+        this.form['months']=5;
+      break;
+      case '22':
+      case '23':
+      case '24':
+      case '25':
+      case '26':
+        this.form['months']=6;
+      break;
+      case '27':
+      case '28':
+      case '29':
+      case '30':
+        this.form['months']=7;
+      break;
+      case '31':
+      case '32':
+      case '33':
+      case '34':
+      case '35':
+        this.form['months']=8;
+      break;
+      case '36':
+      case '37':
+      case '38':
+      case '39':
+      case '40':
+      case '41':
+      case '42':
+        this.form['months']=9;
+      break;
+      default:
+        this.form['months']=-1;
+    }
+  }
 }
